f82c8db0cb9649b28ebb9734cb221be29bd64bdf
Josue Mora <josuedavid_99@hotmail.com>
2023-03-13 17:55:59 -0400

    bd created

--- api/src/controllers/getPokemons.js
+++ api/src/controllers/getPokemons.js
@@ -1,27 +1,31 @@
-const axios = requiere("axios")
-const {Pokemon, Type} = requiere('../db')
+const axios = requiere("axios");
+const { Pokemon, Type } = requiere("../db");
 
 const getPokemons = async () => {
-    try {
-       const response = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=100")
-       const urls = await response.data.results?.map(pokemon => axios.get(pokemon.urls))
-       const getAllPokemon = await Promise.all(urls)
-       const result = getAllPokemon.map((pokemon)=>{
-        return {
-            id: pokemon.data.id,
-            name: pokemon.data.name,
-            image: pokemon.data.sprites.other["official-artwok"]["front_default"],
-            hp: pokemon.data.stats[0].base_stat,
-            attack: pokemon.data.stats[1].base_stat,
-            defense: pokemon.data.stats[2].base_stat,
-            speed: pokemon.data.stats[5].base_stat,
-            height: pokemon.data.height,
-            weight: pokemon.data.weight,
-            types: pokemon.data.types.map((type) => type.type.name)
-        
-        }})
-       return result
-    } catch (error) {
-        throw new Error ("cannot get all pokemons");  
-    }
-}
+  try {
+    const response = await axios.get(
+      "https://pokeapi.co/api/v2/pokemon?limit=100"
+    );
+    const urls = await response.data.results?.map((pokemon) =>
+      axios.get(pokemon.urls)
+    );
+    const getAllPokemon = await Promise.all(urls);
+    const result = getAllPokemon.map((pokemon) => {
+      return {
+        id: pokemon.data.id,
+        name: pokemon.data.name,
+        image: pokemon.data.sprites.other["official-artwok"]["front_default"],
+        hp: pokemon.data.stats[0].base_stat,
+        attack: pokemon.data.stats[1].base_stat,
+        defense: pokemon.data.stats[2].base_stat,
+        speed: pokemon.data.stats[5].base_stat,
+        height: pokemon.data.height,
+        weight: pokemon.data.weight,
+        types: pokemon.data.types.map((type) => type.type.name),
+      };
+    });
+    return result;
+  } catch (error) {
+    throw new Error("cannot get all pokemons");
+  }
+};
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,12 +2,12 @@ const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 const pokemons = require('./Pokemons.js')
-
+const type = requiere('./Types.js')
 
 const router = Router();
 
 // Configurar los routers
-// Ejemplo: router.use('/auth', authRouter);
+
 
 
 module.exports = router;
