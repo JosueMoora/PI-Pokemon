37c4d09a84847f68573069b68f93f1502ed24b16
Josue Mora <josuedavid_99@hotmail.com>
2023-03-29 00:48:52 -0400

    bugs fixed

--- client/src/index.js
+++ client/src/index.js
@@ -8,7 +8,9 @@ import { Provider } from "react-redux";
 import store from "./redux/store";
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:3001/";
+
+axios.defaults.baseURL = "pi-pokemon-production-7017.up.railway.app";
+// axios.defaults.baseURL = "http://localhost:3001/";
 
 ReactDOM.render(
   <React.StrictMode>
